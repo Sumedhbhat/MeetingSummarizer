6ef6e3124ad131e892414d6dce8d4f3ef8e138c2
Sumedh Bhat <sumedhbhat01@gmail.com>
2022-11-27 22:46:35 +0530

    Changes recognized function & returned result

--- PythonApplication/SpeechToTextConverter/speech_to_text_converter.py
+++ PythonApplication/SpeechToTextConverter/speech_to_text_converter.py
@@ -2,22 +2,32 @@ import whisper
 import os
 import time
 
-recognizedText = []
+recognized_text = []
 
 
 def recognize_speech(audioFile, newFile):
     file_path = os.path.join(os.curdir, audioFile) 
+    model = whisper.load_model("base")
+    result = model.transcribe(file_path, language='en')
+    if newFile or len(recognized_text) == 0:
+        recognized_text.append(result['text'])
+    else:
+        recognized_text[len(recognized_text)-1] += result['text']
+    return result['text']
     #print(os.path.exists(file_path))
     #print(file_path)
     #print(os.path.exists(file_path))
 
-# Speech Recognition function to convert Speech to Text
-def SpeechRecognition(audioFile, newFile):
-    file_path = os.path.join(os.curdir, audioFile) 
-    model = whisper.load_model("base")
-    result = model.transcribe(file_path, language='en')
-    if newFile or len(recognizedText) == 0:
-        recognizedText.append(result['text'])
-    else:
-        recognizedText[len(recognizedText)-1] += result['text']
-    return recognizedText
+def get_final_speech_output():
+    return recognized_text
+
+# # Speech Recognition function to convert Speech to Text
+# def SpeechRecognition(audioFile, newFile):
+#     file_path = os.path.join(os.curdir, audioFile) 
+#     model = whisper.load_model("base")
+#     result = model.transcribe(file_path, language='en')
+#     if newFile or len(recognizedText) == 0:
+#         recognizedText.append(result['text'])
+#     else:
+#         recognizedText[len(recognizedText)-1] += result['text']
+#     return recognizedText
