78c4d7a8c6e0049110172f909b180651bf24f091
Sumedh Bhat <sumedhbhat01@gmail.com>
2022-11-27 22:45:20 +0530

    Changed Image Compare Module

--- PythonApplication/ComparisonOfImages/image_compare.py
+++ PythonApplication/ComparisonOfImages/image_compare.py
@@ -1,15 +1,16 @@
 from PIL import Image,ImageChops
 import math, operator
+import os
 from functools import reduce
-imgPath1 = 'D:\images/2.jpeg'
-img_1 = Image.open(imgPath1)
-imgPath2 = 'D:\images/15.jpeg'
-img_2 = Image.open(imgPath2)
 
 def rms_diff(im1, im2):
     "Calculate the root-mean-square difference between two images"
+    img_loc1=os.path.join(os.getcwd(),"..","Output","Screenshots",im1)
+    img_loc2=os.path.join(os.getcwd(),"..","Output","Screenshots",im2)
+    image_file1=Image.open(img_loc1)
+    image_file2=Image.open(img_loc2)
 
-    h = ImageChops.difference(im1, im2).histogram()
+    h = ImageChops.difference(image_file1, image_file2).histogram()
 
     # calculate rms
     return math.sqrt(reduce(operator.add,
